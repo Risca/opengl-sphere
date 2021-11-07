1617f90d6e8f3c1e3c1a7ec40262ed99d2bfb161
Patrik Dahlström <risca@powerlamerz.org>
2021-11-07 19:19:35 +0100

    Setup default surface type to OpenGL ES

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "globewidget.h"
 
 #include <QApplication>
+#include <QSurfaceFormat>
 
 int main(int argc, char *argv[])
 {
+    QSurfaceFormat fmt;
+    fmt.setRenderableType(QSurfaceFormat::OpenGLES);
+    QSurfaceFormat::setDefaultFormat(fmt);
+
     QApplication a(argc, argv);
     GlobeWidget w;
     w.show();
